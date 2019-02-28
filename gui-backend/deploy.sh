15bff8dc21dab4c5086bd5b2a8c1be6cd175cd63
calvin zhu <calvinzhu@hotmail.com>
2019-02-27 19:55:20 -0500

    update root-context, fixed one missing css for date time picker

--- gui-backend/deploy.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /home/czhu/jboss/BPMS/rhpam-7.0.0.GA/rhpam-businessCentral_71/standalone/deployments/backend.*
-cp /home/czhu/works/git/bpms7_git/calvin_fork/process-migration-gui/gui-backend/target/backend.war /home/czhu/jboss/BPMS/rhpam-7.0.0.GA/rhpam-businessCentral_71/standalone/deployments/
-touch /home/czhu/jboss/BPMS/rhpam-7.0.0.GA/rhpam-businessCentral_71/standalone/deployments/backend.war.dodeploy
-ls -l /home/czhu/jboss/BPMS/rhpam-7.0.0.GA/rhpam-businessCentral_71/standalone/deployments/
-
